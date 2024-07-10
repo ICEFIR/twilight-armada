3b2ef3ea8cd16ae20825eadcdd1ed4e8a2baf9fd
ICEFIR <icefir1996@gmail.com>
2024-07-10 03:38:51 -0700

    custom dock minimum implementation

--- better-terrain-faster/src/editor.rs
+++ better-terrain-faster/src/editor.rs
@@ -1,29 +1,45 @@
 
-struct TwilightArmadaExtension;
-use godot::prelude::*;
+use std::borrow::BorrowMut;
+use std::rc::Rc;
+
+use godot::{engine::EditorInterface, prelude::*};
 use godot::classes::EditorPlugin;
 use serde::{Serialize, Deserialize};
-use godot::engine::IEditorPlugin;
+use godot::engine::{Control, IEditorPlugin};
+
+
 
 #[derive(GodotClass)]
-#[class(base=EditorPlugin)]
+#[class(base=EditorPlugin, tool, init, editor_plugin)]
 struct BetterTerrianFasterEditor {
+    dock_scene: Option<Gd<Control>>,
     base: Base<EditorPlugin>
 }
 
 #[godot_api]
 impl IEditorPlugin for BetterTerrianFasterEditor{
-    fn init(base: Base<EditorPlugin>) -> Self {
-        godot_print!("Hello, world from better terrain faster!"); // Prints to the Godot console
-        
-        Self {
-            base,
-        }
+    fn ready(&mut self) {
+        godot_print!("Better terrain faster Rust Extension Ready!"); // Prints to the Godot console
     }
 
     fn enter_tree(&mut self) {
-        godot_print!("Hello, world from better terrain faster enter tree!"); // Prints to the Godot console
-        
+        godot_print!("Better terrain faster Rust Extension Enter Tree!"); // Prints to the Godot console
+        if let Ok(scene) = try_load::<PackedScene>("res://addons/better-terrain-faster/editor/dock.tscn") {
+            godot_print!("Hello, world from better terrain faster enter tree! Scene: {:?}", scene);
+            let scene_instance = scene.instantiate_as::<Control>();
+            self.base_mut().add_control_to_bottom_panel(scene_instance.clone(), GString::from("Better Terrain Faster"));
+            self.dock_scene = Some(scene_instance);
+        } else {
+            godot_print!("Failed to load scene");
+            return;
+        }
+
     }
 
+    fn exit_tree(&mut self) {
+        godot_print!("Better terrain faster Rust Extension Exit Tree!"); // Prints to the Godot console
+        let dock_scene = &mut self.dock_scene.take().unwrap();
+        self.base_mut().remove_control_from_bottom_panel(dock_scene.clone());
+        dock_scene.queue_free();
+    }
 } 
