3615d14e528de4495ce93642885a2ef1d784f7b5
icefir <icefir1996@gmail.com>
2025-03-19 20:14:55 +1100

    update

--- better-terrain-faster/src/editor.rs
+++ better-terrain-faster/src/editor.rs
@@ -1,9 +1,8 @@
-use std::collections::HashMap;
-use std::time::UNIX_EPOCH;
-
 use godot::classes::{Button, Control, EditorPlugin, IEditorPlugin, TileSet};
 use godot::prelude::*;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::time::UNIX_EPOCH;
 
 static EDITOR_DOCK_PATH: &str = "res://addons/better-terrain-faster/editor/dock.tscn";
 static FILE_WATCH_CHECK_INTERVAL_MS: i32 = 2000;
@@ -27,33 +26,34 @@ struct FileWatchMeta {
 #[godot_api]
 impl IEditorPlugin for BetterTerrainFasterEditor {
     fn make_visible(&mut self, visible: bool) {
-        if self.dock_button.is_some() {
-            self.dock_button.as_mut().unwrap().set_visible(visible);
-        }
+        // if self.dock_button.is_some() {
+        //     self.dock_button.as_mut().unwrap().set_visible(visible);
+        // }
     }
 
     // Edit has incorrect calling signature, so this cannot be implemented in rust
     // https://github.com/godot-rust/gdext/issues/494
     // This call will be delegated to the GDScript side
     fn edit(&mut self, object: Option<Gd<Object>>) {
-        if object.is_none() {
-            return;
-        }
-        let object = object.unwrap();
-        if object.is_class(GString::from("TileMapLayer")) {
-            godot_print!("Better terrain faster editing tile map layer");
-            // self.tile_map_layer = Some(object.cast::<TileMapLayer>().unwrap());
-        } else if object.is_class(GString::from("TileSet")) {
-            godot_print!("Better terrain faster editing tile set");
-            self.tile_set = Some(object.cast::<TileSet>());
-        }
+        // if object.is_none() {
+        //     return;
+        // }
+        // let object = object.unwrap();
+        // if object.is_class(GString::from("TileMapLayer")) {
+        //     godot_print!("Better terrain faster editing tile map layer");
+        //     // self.tile_map_layer = Some(object.cast::<TileMapLayer>().unwrap());
+        // } else if object.is_class(GString::from("TileSet")) {
+        //     godot_print!("Better terrain faster editing tile set");
+        //     self.tile_set = Some(object.cast::<TileSet>());
+        // }
     }
 
     fn handles(&self, object: Gd<Object>) -> bool {
-        if !object.is_instance_valid() {
-            return false;
-        }
-        return object.is_class(GString::from("TileMapLayer")) || object.is_class(GString::from("TileSet"));
+        return false
+        // if !object.is_instance_valid() {
+        //     return false;
+        // }
+        // return object.is_class(GString::from("TileMapLayer")) || object.is_class(GString::from("TileSet"));
     }
 
     /* Setup only handles TileMayLayers */
@@ -84,84 +84,84 @@ impl IEditorPlugin for BetterTerrainFasterEditor {
 #[godot_api]
 impl BetterTerrainFasterEditor {
     fn setup_file_watch(&mut self) {
-        self.file_watch.insert(EDITOR_DOCK_PATH.to_string(), load_file_metadata(EDITOR_DOCK_PATH));
+        // self.file_watch.insert(EDITOR_DOCK_PATH.to_string(), load_file_metadata(EDITOR_DOCK_PATH));
     }
 
     fn file_watch_check(&mut self, delta: f64) {
-        self.file_watch_countdown_timer -= (delta * 1000.0) as i32;
-        if self.file_watch_countdown_timer >= 0 {
-            return;
-        }
-        self.file_watch_countdown_timer = FILE_WATCH_CHECK_INTERVAL_MS;
-        // godot_print!("Checking for file changes");
-        let mut modified: bool = false;
-        for (file_path, file_meta) in self.file_watch.clone().iter() {
-            let metadata = load_file_metadata(file_path);
-            if metadata.last_modified != file_meta.last_modified {
-                godot_print!("File has been modified: {}, recorded last modified {}, actual last modified {}", file_path, file_meta.last_modified, metadata.last_modified);
-                modified = true;
-                self.file_watch.insert(file_path.to_string(), metadata);
-            }
-        }
-        if modified {
-            self.reload_files();
-        }
+        // self.file_watch_countdown_timer -= (delta * 1000.0) as i32;
+        // if self.file_watch_countdown_timer >= 0 {
+        //     return;
+        // }
+        // self.file_watch_countdown_timer = FILE_WATCH_CHECK_INTERVAL_MS;
+        // // godot_print!("Checking for file changes");
+        // let mut modified: bool = false;
+        // for (file_path, file_meta) in self.file_watch.clone().iter() {
+        //     let metadata = load_file_metadata(file_path);
+        //     if metadata.last_modified != file_meta.last_modified {
+        //         godot_print!("File has been modified: {}, recorded last modified {}, actual last modified {}", file_path, file_meta.last_modified, metadata.last_modified);
+        //         modified = true;
+        //         self.file_watch.insert(file_path.to_string(), metadata);
+        //     }
+        // }
+        // if modified {
+        //     self.reload_files();
+        // }
     }
 
     fn reload_files(&mut self) {
-        godot_print!("Better terrian faster reloading files");
-        self.unload_dock_ui();
-        self.load_dock_ui();
+        // godot_print!("Better terrian faster reloading files");
+        // self.unload_dock_ui();
+        // self.load_dock_ui();
     }
 
     fn load_dock_ui(&mut self) {
-        godot_print!("Loading better terrain faster scene");
-        let scene = match try_load::<PackedScene>(EDITOR_DOCK_PATH) {
-            Ok(s) => s,
-            Err(_) => {
-                godot_print!("Failed to load scene");
-                return;
-            }
-        };
-
-        let scene_instance = match scene.try_instantiate_as::<Control>() {
-            Some(instance) => instance,
-            None => {
-                godot_print!("Failed to instantiate scene");
-                return;
-            }
-        };
-
-        let mut dock_button = self.base_mut().add_control_to_bottom_panel(
-            scene_instance.clone(),
-            GString::from("Better Terrain Faster"),
-        );
-
-        if let Some(button) = dock_button.as_mut() {
-            button.set_visible(false);
-            self.dock_button = dock_button;
-            self.dock_scene_instance = Some(scene_instance);
-        } else {
-            godot_print!("Failed to add control to bottom panel");
-        }
+        // godot_print!("Loading better terrain faster scene");
+        // let scene = match try_load::<PackedScene>(EDITOR_DOCK_PATH) {
+        //     Ok(s) => s,
+        //     Err(_) => {
+        //         godot_print!("Failed to load scene");
+        //         return;
+        //     }
+        // };
+        //
+        // let scene_instance = match scene.try_instantiate_as::<Control>() {
+        //     Some(instance) => instance,
+        //     None => {
+        //         godot_print!("Failed to instantiate scene");
+        //         return;
+        //     }
+        // };
+        //
+        // let mut dock_button = self.base_mut().add_control_to_bottom_panel(
+        //     scene_instance.clone(),
+        //     GString::from("Better Terrain Faster"),
+        // );
+        //
+        // if let Some(button) = dock_button.as_mut() {
+        //     button.set_visible(false);
+        //     self.dock_button = dock_button;
+        //     self.dock_scene_instance = Some(scene_instance);
+        // } else {
+        //     godot_print!("Failed to add control to bottom panel");
+        // }
     }
 
     fn unload_dock_ui(&mut self) {
-        godot_print!("Unloading better terrain faster dock ui");
-        if self.dock_button.is_some() {
-            self.dock_button.take().unwrap().queue_free();
-        }
-        let mut dock_scene_instance_option = self.dock_scene_instance.take();
-        match &mut dock_scene_instance_option {
-            Some(dock_scene_instance) => {
-                self.base_mut()
-                    .remove_control_from_bottom_panel(dock_scene_instance.clone());
-                dock_scene_instance.queue_free();
-            }
-            None => {
-                godot_print!("Dock scene is already unloaded");
-            }
-        }
+        // godot_print!("Unloading better terrain faster dock ui");
+        // if self.dock_button.is_some() {
+        //     self.dock_button.take().unwrap().queue_free();
+        // }
+        // let mut dock_scene_instance_option = self.dock_scene_instance.take();
+        // match &mut dock_scene_instance_option {
+        //     Some(dock_scene_instance) => {
+        //         self.base_mut()
+        //             .remove_control_from_bottom_panel(dock_scene_instance.clone());
+        //         dock_scene_instance.queue_free();
+        //     }
+        //     None => {
+        //         godot_print!("Dock scene is already unloaded");
+        //     }
+        // }
     }
 }
 
